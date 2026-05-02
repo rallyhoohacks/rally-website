fac8eb6ddfe833a9adfac641eeddef7edc91a01c
Tyler Nguyen <wgq4tr@virginia.edu>
2026-05-02 16:33:42 -0400

    made rally button clickable and send you to the top

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,14 @@
+import { lenis } from './lenis'
 import './Header.css'
 
 export default function Header() {
   return (
     <header className="header">
-      <span className="header-logo">
+      <span
+        className="header-logo"
+        style={{ cursor: 'pointer' }}
+        onClick={() => lenis.scrollTo(0)}
+      >
         R<span className="logo-green">all</span>y
       </span>
     </header>
